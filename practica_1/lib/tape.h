ada1802cf9ab234ea93c0770cbdd733a521ae87b
javicg09 <alu0101333021@ull.edu.es>
2026-02-12 12:08:57 +0000

    P1 casi terminada

--- practica_1/lib/tape.h
+++ practica_1/lib/tape.h
@@ -1,22 +1,32 @@
-#ifndef _TAPE_
-#define _TAPE_
-#include <fstream>
+#ifndef TAPE_H_
+#define TAPE_H_
+
 #include <iostream>
 #include <vector>
 
+// Representa la cinta bidimensional de celdas sobre la que se mueve la hormiga.
+// Se encarga de almacenar los colores y de la visualización.
 class Tape {
  public:
+  // Inicializa la cinta con tamaño fijo, por defecto todas blancas (0).
   Tape(int x, int y);
-  int getColor(int x, int y) const;
-  void setColor(int x, int y, int color);
 
-  int getsizeX();
-  int gestsizeY();
+  // Consulta el color de una celda (0 para blanca, 1 para negra).
+  int GetColor(int x, int y) const;
+  
+  // Modifica el color de una celda.
+  void SetColor(int x, int y, int color);
 
-  friend std::ostream& operator<<(std::ostream& os, const Tape& t);
+  int get_size_x() const;
+  int get_size_y() const;
+
+  // Sobrecarga para mostrar la cinta: blancas con ' ' y negras con 'x'.
+  friend std::ostream& operator<<(std::ostream& os, const Tape& tape);
 
  private:
-  int sizeX_, sizeY_;
+  int size_x_;
+  int size_y_;
   std::vector<std::vector<int>> color_;
 };
-#endif
+
+#endif  // TAPE_H_
--- practica_1/src/tape.cpp
+++ practica_1/src/tape.cpp
@@ -1,18 +1,17 @@
 #include "../lib/tape.h"
 
-Tape::Tape(int x, int y) {
-  sizeX_ = x;
-  sizeY_ = y;
-  color_.assign(y, std::vector<int>(x, 0));
+Tape::Tape(int x, int y) : size_x_(x), size_y_(y) {
+  // Inicializa el vector de vectores con valor 0 (blanco).
+  color_.assign(size_y_, std::vector<int>(size_x_, 0));
 }
 
-int Tape::getColor(int x, int y) const { return color_[y][x]; }
+int Tape::GetColor(int x, int y) const { return color_[y][x]; }
 
-void Tape::setColor(int x, int y, int color) { color_[y][x] = color; }
+void Tape::SetColor(int x, int y, int color) { color_[y][x] = color; }
 
-int Tape::getsizeX() { return sizeX_; }
+int Tape::get_size_x() const { return size_x_; }
 
-int Tape::gestsizeY() { return sizeY_; }
+int Tape::get_size_y() const { return size_y_; }
 
 std::ostream& operator<<(std::ostream& os, const Tape& t) {
   for (const std::vector<int>& row : t.color_) {
