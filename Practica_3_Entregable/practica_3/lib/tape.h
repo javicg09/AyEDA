831e6c7e38fa6da9bb71871035696d1f328ff87e
javicg09 <alu0101333021@ull.edu.es>
2026-03-09 12:45:04 +0000

    Modificación de la clase: Tape

--- Practica_3_Entregable/practica_3/lib/tape.h
+++ Practica_3_Entregable/practica_3/lib/tape.h
@@ -4,37 +4,27 @@
 #include <iostream>
 #include <vector>
 
-// Aunque usamos int internamente para n colores, el enum ayuda a la legibilidad.
-enum class Color : int { kColor0 = 0, kColor1 = 1, kColor2 = 2, kColor3 = 3 };
-
-// Representa la cinta bidimensional de celdas sobre la que se mueve la hormiga.
-// Se encarga de almacenar los colores y de la visualización.
+// Representa la clase base de la cinta
 class Tape {
  public:
   // Inicializa la cinta con tamaño fijo, por defecto todas blancas (0).
   Tape(int x, int y, int num_colors);
+  virtual ~Tape() {}
 
   // Consulta el color de una celda (0 para blanca, 1 para negra).
-  int GetColor(int x, int y) const;
-  
-  // Modifica el color de una celda.
-  void SetColor(int x, int y, int color);
+  virtual int GetColor(int x, int y) const = 0;
 
   // Incrementa el color de la celda de forma cíclica (0 -> 1 -> ... -> n-1 -> 0).
-  void NextColor(int x, int y);
+  virtual void NextColor(int x, int y) = 0;
 
   int get_size_x() const;
   int get_size_y() const;
   int get_num_colors() const;
 
-  // Sobrecarga para mostrar la cinta: blancas con ' ' y negras con 'x'.
-  friend std::ostream& operator<<(std::ostream& os, const Tape& tape);
-
- private:
+ protected:
   int size_x_;
   int size_y_;
   int num_colors_;
-  std::vector<std::vector<int>> color_;
 };
 
 #endif  // TAPE_H_
--- Practica_3_Entregable/practica_3/src/tape.cpp
+++ Practica_3_Entregable/practica_3/src/tape.cpp
@@ -1,33 +1,9 @@
 #include "../lib/tape.h"
 
-Tape::Tape(int x, int y, int num_colors) : size_x_(x), size_y_(y), num_colors_(num_colors) {
-  // Inicializa el vector de vectores con valor 0 (blanco).
-  color_.assign(size_y_, std::vector<int>(size_x_, 0));
-}
-
-int Tape::GetColor(int x, int y) const { return color_[y][x]; }
-
-void Tape::SetColor(int x, int y, int color) { color_[y][x] = color; }
-
-void Tape::NextColor(int x, int y) {
-  if (y >= 0 && y < size_y_ && x >= 0 && x < size_x_) {
-    // Incremento cíclico: (color + 1) MOD n.
-    color_[y][x] = (color_[y][x] + 1) % num_colors_;
-  }
-}
+Tape::Tape(int x, int y, int num_colors) : size_x_(x), size_y_(y), num_colors_(num_colors) {}
 
 int Tape::get_size_x() const { return size_x_; }
 
 int Tape::get_size_y() const { return size_y_; }
 
 int Tape::get_num_colors() const { return num_colors_; }
-
-std::ostream& operator<<(std::ostream& os, const Tape& t) {
-  for (const std::vector<int>& row : t.color_) {
-    for (int cell : row) {
-      os << (cell == 0 ? '0' : '1');
-    }
-    os << "\n";
-  }
-  return os;
-}
