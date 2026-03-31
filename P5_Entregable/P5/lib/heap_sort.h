c871dce2ff7a92d620f46cefee19c1e9ab1f2e10
javicg09 <alu0101333021@ull.edu.es>
2026-03-31 17:36:00 +0100

    P5: HeapSort

---
+++ P5_Entregable/P5/lib/heap_sort.h
@@ -0,0 +1,34 @@
+#ifndef _HEAP_SORT_H_
+#define _HEAP_SORT_H_
+
+#include "static_sequence.h"
+#include <algorithm>
+
+template<class Key>
+void sink(StaticSequence<Key>& seq, unsigned size, unsigned i){
+  unsigned max = i;
+  unsigned left = (2*i) + 1;
+  unsigned right = (2*i) + 2;
+
+  if (left < size && seq[left] > seq[max]) max = left;
+  if (right < size && seq[right] > seq[max]) max = right;
+
+  if (max != i) {
+    std::swap(seq[i], seq[max]);
+    sink(seq, size, max);
+  }
+}
+
+template<class Key>
+void heapSort(StaticSequence<Key>& seq, unsigned size) {
+  unsigned mid = (size / 2) - 1;
+  for (int i = mid; i >= 0; i--){
+    sink(seq, size, i);
+  }
+  for (int i = size - 1; i > 0; i--){
+    std::swap(seq[0], seq[i]);
+    sink(seq, i, 0);
+  }
+}
+
+#endif
