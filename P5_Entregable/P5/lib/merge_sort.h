e1769662cdd3de8a5b3d861f39b713ac3d676803
javicg09 <alu0101333021@ull.edu.es>
2026-03-31 17:16:07 +0100

    P5: MergeSort

---
+++ P5_Entregable/P5/lib/merge_sort.h
@@ -0,0 +1,41 @@
+#ifndef _MERGE_SORT_H_
+#define _MERGE_SORT_H_
+
+#include "static_sequence.h"
+#include <algorithm>
+
+template<class Key>
+void mix(StaticSequence<Key>& seq, unsigned begin, unsigned mid, unsigned end) {
+  Key aux[end - begin + 1];
+  int i = begin; j = mid + 1; k = 0;
+  while (i <= mid && j <= end) {
+    if (seq[i] < seq[j]) {
+        aux[k] = seq[i];
+        i++;
+    } else{
+      aux[k] = seq[j];
+      j++;
+    }
+    k++;
+  }
+  // copiar restos
+  while (i <= mid) aux[k++] = seq[i++];
+  while (j <= end) aux[k++] = seq[j++];
+
+  // copiar aux de vuelta a seq
+  for (unsigned l = begin; l <= end; l++) {
+    seq[l] = aux[l - begin];
+  }
+}
+
+template<class Key>
+void mergeSort(StaticSequence<Key>& seq, unsigned begin, unsigned end) {
+  if (begin < end) {
+    unsigned mid = (begin + end) / 2;
+    mergeSort(seq, begin, mid);
+    mergeSort(seq, mid + 1, end);
+    mix(seq, begin, mid, end);
+  }
+}
+
+#endif
